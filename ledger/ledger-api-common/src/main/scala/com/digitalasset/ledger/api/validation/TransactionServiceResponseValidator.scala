dc62209a6038e0bcc732a7629d2adf4685a47e66
tudor-da <tudor.voicu@digitalasset.com>
2021-01-14 15:20:30 +0200

    WIP

---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/ledger/api/validation/TransactionServiceResponseValidator.scala
@@ -0,0 +1,30 @@
+package com.daml.ledger.api.validation
+
+import com.daml.ledger.api.domain.{LedgerId, LedgerOffset}
+import com.daml.lf.data.Ref.LedgerString
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class TransactionServiceResponseValidator(
+    ledgerId: LedgerId,
+    fetchLedgerEnd: String => Future[LedgerOffset.Absolute],
+)(implicit ec: ExecutionContext) {
+  def validate[Response](
+      response: Response,
+      offsetFromResponse: Response => LedgerString,
+      offsetOrdering: Ordering[LedgerOffset.Absolute],
+  ): Future[Response] =
+    for {
+      ledgerEnd <- fetchLedgerEnd(ledgerId.toString)
+      offset = offsetFromResponse(response)
+      ledgerOffset = LedgerOffset.Absolute(offset)
+      validatedResponse <- LedgerOffsetValidator
+        .offsetIsBeforeEndIfAbsolute(
+          offsetType = "End",
+          ledgerOffset = ledgerOffset,
+          ledgerEnd = ledgerEnd,
+          offsetOrdering = offsetOrdering,
+        )
+        .fold(Future.failed, _ => Future.successful(response))
+    } yield validatedResponse
+}
